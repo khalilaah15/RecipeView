fcf562bd1a6cacab99268953834b1be3d1e81e29
khalilaah15 <khalilullah_nuraini_30rpl@student.smktelkom-mlg.sch.id>
2023-02-07 10:17:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recipeview/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.recipeview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setLogo(R.drawable.back)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayUseLogoEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
